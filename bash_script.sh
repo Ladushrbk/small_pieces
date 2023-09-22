1a6f993622eb272090749f5077caf6416d6ce4f3
Lada Hrbková <114622971+Ladushrbk@users.noreply.github.com>
2023-09-22 15:06:10 +0200

    Create bash_script.sh
    Added new working code

---
+++ bash_script.sh
@@ -0,0 +1,10 @@
+line="...................................................."
+echo "Starting at: $(date)"; echo $line
+
+echo "uptime";uptime;echo $line
+
+echo "free";free;echo $line
+
+echo "who";who;echo $line
+
+echo "Finishing at: $(date)"
